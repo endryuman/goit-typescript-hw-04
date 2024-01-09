94602c8eedde8e2f251d66a381571f6a1de4951b
endryuman <132812966+endryuman@users.noreply.github.com>
2024-01-09 17:12:37 +0200

    finish

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -6,18 +6,30 @@ type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
 
+type SelectedMenu = {
+  id: MenuIds;
+};
+
+type MenuSelected = {
+  selectedMenu: SelectedMenu;
+};
+
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+  selectedMenu: { id: "first" },
 });
 
 // Додайте тип MenuAction
+
+type MenuAction = {
+  onSelectedMenu(object: SelectedMenu): void;
+};
 
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode;
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +60,8 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[];
+  // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
