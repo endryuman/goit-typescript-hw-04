67fd9a0e79e1b8486330e6ef19dd416a519f5860
endryuman <132812966+endryuman@users.noreply.github.com>
2024-01-10 04:17:44 +0200

    done

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -2,7 +2,7 @@ import React, { useReducer } from "react";
 
 interface State {
   isRequestInProgress: boolean;
-  requestStep: string;
+  requestStep: "idle" | "start" | "pending" | "finished";
 }
 
 type Action = {
--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -34,7 +34,9 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({
+    id: "first",
+  });
 
   const menuContextAction = useMemo(
     () => ({
